fad594a2ea9610148b8dfd0ecfe17de6345325f7
Michael Khoroshun <michael.khoroshun@gmail.com>
2017-08-04 23:50:40 +0300

    Add security

---
+++ src/main/java/stopheracleum/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,10 @@
+package stopheracleum.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by michael on 03.08.17.
+ */
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
