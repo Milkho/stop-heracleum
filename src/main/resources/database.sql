e755fdf87591f549c21908e3531372b2b674e4ed
milkho <michael.khoroshun@gmail.com>
2017-08-05 23:54:19 +0300

    Add security

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -36,6 +36,7 @@ CREATE TABLE IF NOT EXISTS point (
   ENGINE = InnoDB;
 
 -- Insert data
+
 INSERT INTO users VALUES (1, 'admin', '$2a$11$uSXS6rLJ91WjgOHhEGDx..VGs7MkKZV68Lv5r1uwFu7HgtRn3dcXG'
 );
 
