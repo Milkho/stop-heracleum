cb31d9a087d63420187583941ff0d5cf79519e73
milkho <michael.khoroshun@gmail.com>
2017-08-07 23:31:26 +0300

    Reorganize service layer

--- src/main/java/stopheracleum/service/UserService.java
+++ src/main/java/stopheracleum/service/UserService.java
@@ -2,24 +2,13 @@ package stopheracleum.service;
 
 import stopheracleum.model.User;
 
-import java.util.List;
-
 /**
- * Created by michael on 02.08.17.
+ * Service class for {@link stopheracleum.model.User}
  */
+
 public interface UserService {
-    User findById(int id);
 
-    User findBySSO(String sso);
+    void save(User user);
 
-    void saveUser(User user);
-
-    void updateUser(User user);
-
-    void deleteUserBySSO(String sso);
-
-    List<User> findAllUsers();
-
-    boolean isUserSSOUnique(Integer id, String sso);
-
+    User findByUsername(String username);
 }
