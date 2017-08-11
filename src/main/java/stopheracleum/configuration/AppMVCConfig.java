f694272e39e215faa30f529c9b902f9c3b379cf2
milkho <michael.khoroshun@gmail.com>
2017-08-11 22:55:55 +0300

    Add configuration files

--- src/main/java/stopheracleum/configuration/AppMVCConfig.java
+++ src/main/java/stopheracleum/configuration/AppMVCConfig.java
@@ -9,6 +9,10 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+/**
+ * Class that configure working with resources
+ */
+
 @Configuration
 @EnableWebMvc
 public class AppMVCConfig extends WebMvcConfigurerAdapter {
